167cd6e15b72c7d45c6d13f392a00df0619dcf33
Msmzjss <100003952+Msmzjss@users.noreply.github.com>
2022-02-19 09:16:28 -0300

    Add files via upload

--- assets/preload/data/high-shovel/script.lua
+++ assets/preload/data/high-shovel/script.lua
@@ -1,1 +1,6 @@
-
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'shadowded'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'ShadowDies'); --put in sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in music/
+end
