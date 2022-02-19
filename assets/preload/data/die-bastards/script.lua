0d38656873069f08c53382422d4abcaca4df585b
Msmzjss <100003952+Msmzjss@users.noreply.github.com>
2022-02-19 09:10:27 -0300

    Add files via upload

--- assets/preload/data/die-bastards/script.lua
+++ assets/preload/data/die-bastards/script.lua
@@ -1,1 +1,6 @@
-
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bfTrollgundead'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'GunDeath'); --put in sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in music/
+end
